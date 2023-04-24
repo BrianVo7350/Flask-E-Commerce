bd24b462527eef7bf900ad9bacdffe4dbb7d10f1
Tyler OBrien <tylerob65@gmail.com>
2023-04-23 22:34:13 -0400

    Add routes to modify cart and show cart

--- app/models.py
+++ app/models.py
@@ -13,6 +13,8 @@ class Users(db.Model,UserMixin):
     last_name = db.Column(db.String(50))
     date_joined = db.Column(db.DateTime,nullable = False, default=datetime.utcnow())
 
+    cart_items = db.relationship("Carts",foreign_keys='Carts.user_id',back_populates="user")
+
     def __init__(self, email, password, first_name,last_name):
         self.email = email
         self.password = password
@@ -27,12 +29,16 @@ class Users(db.Model,UserMixin):
         db.session.delete(self)
         db.session.commit()
 
+
+
 class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(100), nullable = False, unique=True)
     price = db.Column(db.Float, nullable = False)
     description = db.Column(db.String(500), nullable = False)
     image_url = db.Column(db.String)
+
+    associated_carts = db.relationship("Carts",foreign_keys='Carts.product_id',back_populates="product")
 
     def __init__(self, product_name, price, description, image_url):
         self.product_name = product_name
@@ -48,5 +54,27 @@ class Products(db.Model):
         db.session.delete(self)
         db.session.commit()
     
+class Carts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_quantity = db.Column(db.Integer,nullable=False)
     
+    user_id = db.Column(db.Integer, db.ForeignKey(Users.id),nullable=False)
+    user = db.relationship("Users",back_populates="cart_items",foreign_keys=[user_id])
+    
+    product_id = db.Column(db.Integer, db.ForeignKey(Products.id),nullable=False)
+    product = db.relationship("Products",back_populates="associated_carts",foreign_keys=[product_id])
+    
+    def __init__(self, user_id, product_id, item_quantity):
+        self.user_id = user_id
+        self.product_id = product_id
+        self.item_quantity = item_quantity
 
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def deleteFromDB(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    
